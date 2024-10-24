89a28f11d3253fd4f16119da834450552b2b8285
Kiranjit-2122 <155612497+Kiranjit-2122@users.noreply.github.com>
2024-10-24 19:15:06 +0530

    Add files via upload
    React Input Handling and React Router DOM
    This project demonstrates handling form inputs using useState and setting up navigation using React Router DOM. It includes examples of capturing user input in real time and navigating between different components.

---
+++ Forms.js
@@ -0,0 +1,39 @@
+import { useState } from "react"
+export default function Forms(){
+    var [name,setName]=useState("")
+    var [email,setEmail]=useState("")
+    var [password,setPassword]=useState("")
+    var [contact,setContact]=useState("")
+
+    const changeName=(e)=>{
+        console.log(e.target.value);
+        setName(e.target.value)
+    }
+
+    const changeEmail=(e)=>{
+        setEmail(e.target.value)
+    }
+    return(
+        <>
+        <label>Name:</label>
+        <input value={name} onChange={changeName}></input>
+        {name}
+        <br/>
+        <br/>
+        <label>Email:</label>
+        <input value={email} onChange={changeEmail}></input>
+        {email}
+        <br/>
+        <br/>
+        <label>Password:</label>
+        <input value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
+        {password}
+        <br/>
+        <br/>
+
+        <label>Contact Number</label>
+        <input value={contact} onChange={(e)=>{setContact(e.target.value)}}></input>
+        {contact}
+        </>
+    )
+}
